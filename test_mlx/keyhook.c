f695bccf6d9ac76b2f0e10893f0e8437b17c929f
Othmane <obouhoura@gmail.com>
2025-04-06 15:41:41 +0200

    Ajout des deplacement avant/arriere

--- test_mlx/keyhook.c
+++ test_mlx/keyhook.c
@@ -6,7 +6,7 @@
 /*   By: obouhour <obouhour@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/05 15:26:13 by obouhour          #+#    #+#             */
-/*   Updated: 2025/04/06 15:02:44 by obouhour         ###   ########.fr       */
+/*   Updated: 2025/04/06 15:40:16 by obouhour         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,15 +14,28 @@
 
 int	handle_keyhook(int keycode, t_game *game)
 {
-	printf("Touche pressée: %d\n", keycode);
-	if (keycode = 65363) //f/droite
-	if (keycode = 65361) //f/gauche
-	if (keycode = 97)//a
-	if (keycode == 115)//s
-	if (keycode == 100)//d
-	if (keycode == 119)//w
+	double	old_dir;
+	double	old_plane;
 
 	if (keycode == 65307)//echap
 		close_window(game);
+	if (keycode == 65363) //f/droite
+	{
+
+	}
+	if (keycode == 65361) //f/gauche
+	{
+
+	}
+	if (keycode == 119)//w
+	{
+		game->player->pos_x += game->player->dir_x + MOVE_SPD;
+		game->player->pos_y += game->player->dir_y + MOVE_SPD;
+	}
+	if (keycode == 115)//s
+	{
+		game->player->pos_x -= game->player->dir_x + MOVE_SPD;
+		game->player->pos_y -= game->player->dir_y + MOVE_SPD;
+	}
 	return (1);
 }
