0a1dcd3631e912a7206ea01e7739a934cb99b1f7
Othmane <obouhoura@gmail.com>
2025-04-06 12:23:03 +0200

    Test cos et sin

---
+++ test_raycasting/test_sin_cos.c
@@ -0,0 +1,46 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   test_sin_cos.c                                     :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: obouhour <obouhour@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/04/02 12:23:07 by obouhour          #+#    #+#             */
+/*   Updated: 2025/04/06 12:03:00 by obouhour         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#define PI 3.14159265358979323846
+#include <stdlib.h>
+#include <stdio.h>
+#include <math.h>
+
+int	main(void)
+{
+	double	i = 45.0;
+	double	radian;
+
+	radian = i * PI / 180.0;
+	printf("%f\n", cos(radian));
+	printf("%f\n", sin(radian));
+	return(0);
+}
+
+/*pour la lib math ne pas oublier le flag -lm*/
+
+/*
+En mathématiques, il existe deux unités principales pour mesurer les angles :
+
+Les degrés (°) : de 0° à 360°
+Les radians (rad) : de 0 à 2π
+
+Pourquoi diviser par 180 ?
+
+Un cercle complet fait 360° en degrés
+Le même cercle fait 2π en radians
+Donc : 180° = π radians
+
+Formule degre en radians:
+rad = deg * (PI/180)
+
+*/
